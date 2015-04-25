0e4a285cf8e2a1e65e291c71783ebbfcfeb5eab2
Alex <dino0315@gmail.com>
2015-04-25 15:47:09 +0800

    實作 jQueryController 與 each 動作方法，並讓網址變成 http://localhost/jQuery.each 還要確保全站可正常瀏覽

--- MVC5Course/App_Start/RouteConfig.cs
+++ MVC5Course/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace MVC5Course
             routes.MapRoute(
                 name: "Default",
                 //url: "{controller}/{action}/{id}",
-                url: "Docs/{controller}-{action}/{id}",
+                url: "{controller}.{action}/{id}",
                 defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
             );
         }
