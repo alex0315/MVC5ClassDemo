75ade715bae54a127d2a5b5614fa047333fcd282
Alex <dino0315@gmail.com>
2015-04-26 10:44:26 +0800

    32 練習 FileResult 的各種用法 ( 檔案路徑, 指定 Content-Type, byte[], 強制下載檔案 )  33 練習 JsonResult 的用法

--- MVC5Course/Controllers/ARController.cs
+++ MVC5Course/Controllers/ARController.cs
@@ -6,12 +6,73 @@ using System.Web.Mvc;
 
 namespace MVC5Course.Controllers
 {
-    public class ARController : Controller
+    public class ARController : BaseController
     {
         // GET: AR
-        public ActionResult Index()
+        public ActionResult Index(string id)
+        {
+            //if (id.HasValue)
+            //{
+            //    if (id.Value == 1)
+            //    {
+            //        return View("View1");
+            //    }
+                
+            //}
+
+            return View();
+        }
+
+        public ActionResult View1()
         {
             return View();
         }
+
+        public ActionResult View2()
+        {
+            return PartialView();
+        }
+
+        public ActionResult File1()
+        {
+            var data = System.IO.File.ReadAllBytes(Server.MapPath("~/Content/pic1.jpg"));
+
+            return File(data, "image/jpg");
+        }
+
+        public ActionResult File2()
+        {
+            return File(Server.MapPath("~/Content/pic1.jpg"), "image/jpg");
+        }
+
+        public ActionResult File3()
+        {
+            var data = System.IO.File.ReadAllBytes(Server.MapPath("~/Content/pic1.jpg"));
+
+            return File(data, "image/jpg", "艦娘.jpg");
+        }
+
+        public ActionResult File4()
+        {
+            var data = System.IO.File.ReadAllBytes(Server.MapPath("~/Content/bootstrap.css"));
+
+            return File(Server.MapPath("~/Content/bootstrap.css"), "text/plain");
+        }
+
+        public ActionResult File5()
+        {
+            var data = System.IO.File.ReadAllBytes(Server.MapPath("~/Content/pic1.jpg"));
+
+            return File(data, "image/jpg");
+        }
+
+        public ActionResult Json1()
+        {
+            db.Configuration.LazyLoadingEnabled = false;
+            var data = db.Product.Take(10);
+
+            return Json(data, JsonRequestBehavior.AllowGet);
+        }
+
     }
 }
--- MVC5Course/Controllers/BaseController.cs
+++ MVC5Course/Controllers/BaseController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MVC5Course.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,5 +9,6 @@ namespace MVC5Course.Controllers
 {
     public abstract class BaseController : Controller
     {
+        protected FabricsEntities db = new FabricsEntities();
     }
 }
