20696dc45e46f12a810bd7e5f3fd9dc079edfb88
Alex <dino0315@gmail.com>
2015-04-26 17:28:03 +0800

    40 練習 [HandleError] 自訂錯誤頁面的做法

--- MVC5Course/Controllers/AFController.cs
+++ MVC5Course/Controllers/AFController.cs
@@ -13,8 +13,13 @@ namespace MVC5Course.Controllers
             return View();
         }
 
-        public ActionResult MadeMeError()
+        [HandleError(Master = "", ExceptionType = typeof(ExecutionEngineException), View = "Error.ExecutionEngine")]
+        public ActionResult MadeMeError(string type)
         {
+            if (type == "1")
+            {
+                throw new ExecutionEngineException("測試錯誤");
+            }
             throw new Exception("不OK");
             return View();
         }
