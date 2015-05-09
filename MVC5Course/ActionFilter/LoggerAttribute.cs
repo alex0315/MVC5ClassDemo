0797e4fec28b8ac3cc374add348d617f0ee92622
Alex <dino0315@gmail.com>
2015-05-09 09:51:14 +0800

    41 練習自訂 ActionFilter 動作過濾器 42 練習安裝與使用 Glimpse 幫助偵錯 ASP.NET MVC 應用程式

---
+++ MVC5Course/ActionFilter/LoggerAttribute.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVC5Course.ActionFilter
+{
+    public class LoggerAttribute : ActionFilterAttribute
+    {
+        public override void OnActionExecuting(ActionExecutingContext filterContext)
+        {
+            Trace.TraceInformation("ActionExecuting");
+            base.OnActionExecuting(filterContext);
+        }
+
+        public override void OnActionExecuted(ActionExecutedContext filterContext)
+        {
+            Trace.TraceInformation("ActionExecuted");
+            base.OnActionExecuted(filterContext);
+        }
+    }
+}
